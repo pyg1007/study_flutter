1a6bdaa507e41873d64c4c77f6d2aff13febf171
pyg1007 <pyg941007@hanmail.net>
2024-07-28 19:45:54 +0900

    add statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,6 @@ class MyApp extends StatelessWidget {
 /// 한번 그려진 후에는 변하지 않는 위젯
 /// ex) 앱의 로고, 정적인 텍스트 등
 ///
-/// StatefulWidget
-/// 상태를 가지고 있으며, 상태가 변경될 때마다 UI가 업데이트
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -40,6 +38,40 @@ class MyHomePage extends StatelessWidget {
     /// Scaffold
     /// 앱의 기본적인 구조(앱바, 네비게이션 요소, 기본적인 레이아웃 구조), 레이아웃을 정의하고 구성하는데 사용하는 위젯
     ///
-    return Scaffold(body: Text("a"),);
+    return const Scaffold(body: Text("a"));
+  }
+}
+
+/// StatefulWidget
+/// 상태를 가지고 있으며, 상태가 변경될 때마다 UI가 업데이트
+
+class MyHomePageState extends StatefulWidget {
+  const MyHomePageState({super.key});
+
+  @override
+  State createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePageState>{
+
+  String msg = "A";
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// 3초 딜레이
+    /// A -> B로 바꿈
+
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        msg = "B";
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text(msg));
   }
 }
